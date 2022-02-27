670cdd8ff775838858f661f472bab5e923036d34
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2022-02-27 07:46:45 -0300

    aplicando o CSS

--- lets_do_it/src/components/Header/index.jsx
+++ lets_do_it/src/components/Header/index.jsx
@@ -1,5 +1,22 @@
+import "./styles.css";
+
 function Header(){
-    return <h1>Aqui é o cabeçalho da aplicação.</h1>;
+    return (
+        <header id='header'>
+            <nav id='navbar'>
+                <span className='brand'>Let'sDoIt</span>
+
+                <div className='hight-side'>
+                    <img src="https://avatars.githubusercontent.com/u/86314443?s=40&v=4" 
+                    alt="Caliga Profile" />
+                    <div className="profile">
+                        <p className="name"> Caliga</p>
+                        <p className="welcome-message">Bem vindo(a) ao Let'sDoIt</p>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header;
